a6b8d466a840409442dd687b925dfb3d792319ce
vank3f3 <vank3f3@gmail.com>
2019-02-19 17:47:04 +0800

    commit dockerfile base on ubuntu16.04

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,67 +1,40 @@
 #!/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-: ${CONFIG_FILE:=/usr/vpnserver/vpn_server.config}
-: ${LOG_DIR:=/var/log/vpnserver}
 
-: ${DEFAULT_HOST:=localhost}
-: ${DEFAULT_HUB:=DEFAULT}
+mkdir -p /usr/local/vpnserver/config
 
+ln -s /usr/local/vpnserver/config/vpn_server.config \
+      /usr/local/vpnserver/vpn_server.config
+
+if [ ! -d "/var/log/vpnserver/security_log" ]; then
+  mkdir -p /var/log/vpnserver/security_log
+  sleep 2
+fi
+
+if [ ! -d "/var/log/vpnserver/packet_log" ]; then
+  mkdir -p /var/log/vpnserver/packet_log
+  sleep 2
+fi
+
+if [ ! -d "/var/log/vpnserver/server_log" ]; then
+  mkdir -p /var/log/vpnserver/server_log
+  sleep 2
+fi
+
+/usr/local/vpnserver/vpnserver start
+sleep 2
+
+tail -F /usr/local/vpnserver/*_log/*.log &
+sleep 2
+
+/sbin/ip address add 172.16.0.1/24 brd + dev tap_default
+sleep 2
+iptables -t nat -D POSTROUTING -s 172.16.0.0/24 -j MASQUERADE
+sleep 2
+iptables -t nat -A POSTROUTING -s 172.16.0.0/24 -j MASQUERADE
+sleep 2
+
+service dnsmasq  start
+
+set +e
+while pgrep vpnserver > /dev/null; do sleep 1; done
 set -e
-
-if [ ! -d "${LOG_DIR}/security_log" ]; then
-  mkdir -p ${LOG_DIR}/security_log
-fi
-
-if [ ! -d "${LOG_DIR}/packet_log" ]; then
-  mkdir -p ${LOG_DIR}/packet_log
-fi
-
-if [ ! -d "${LOG_DIR}/server_log" ]; then
-  mkdir -p ${LOG_DIR}/server_log
-  ln -s ${LOG_DIR}/*_log /usr/vpnserver/
-fi
-
-if [ ! -f "${CONFIG_FILE}" ]; then
-    : ${USERNAME:=user$(cat /dev/urandom | tr -dc '0-9' | fold -w 4 | head -n 1)}
-    : ${PSK:=notasecret}
-
-    [[ ! $PASSWORD ]] && PASSWORD=$(cat /dev/urandom | tr -dc '0-9' | fold -w 20 | head -n 1 | sed 's/.\{4\}/&./g;s/.$//;')
-    HPW=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w 16 | head -n 1)
-    SPW=$(cat /dev/urandom | tr -dc 'A-Za-z0-9' | fold -w 20 | head -n 1)
-
-    vpnserver start 2>&1 > /dev/null
-
-    while : ; do
-        set +e
-        vpncmd ${DEFAULT_HOST} /server /csv /cmd ServerCipherSet ECDHE-RSA-AES256-GCM-SHA384 2>&1 > /dev/null
-        [[ $? -eq 0 ]] && break
-        set -e && sleep 1
-    done
-
-    vpncmd ${DEFAULT_HOST} /server /csv /hub:${DEFAULT_HUB} /cmd SecureNatEnable
-    vpncmd ${DEFAULT_HOST} /server /csv /cmd IPsecEnable /L2TP:${L2TP:-yes} /L2TPRAW:${L2TPRAW:-yes} /ETHERIP:${ETHERIP:-yes} /PSK:${PSK} /DEFAULTHUB:${DEFAULT_HUB}
-    vpncmd ${DEFAULT_HOST} /server /csv /cmd OpenVpnEnable yes /PORTS:${OPENVPN_PORT:-1194}
-    vpncmd ${DEFAULT_HOST} /server /csv /cmd OpenVpnMakeConfig openvpn.zip >/dev/null
-    vpncmd ${DEFAULT_HOST} /server /csv /cmd SstpEnable yes
-    vpncmd ${DEFAULT_HOST} /server /csv /hub:${DEFAULT_HUB} /cmd UserCreate ${USERNAME} /GROUP:none /REALNAME:none /NOTE:none
-    vpncmd ${DEFAULT_HOST} /server /csv /hub:${DEFAULT_HUB} /cmd UserPasswordSet ${USERNAME} /PASSWORD:${PASSWORD}
-    vpncmd ${DEFAULT_HOST} /server /csv /hub:${DEFAULT_HUB} /cmd SetHubPassword ${HPW}
-    vpncmd ${DEFAULT_HOST} /server /csv /cmd ServerPasswordSet ${SPW}
-
-    printf "# username: ${USERNAME}\n"
-    printf "# password: ${PASSWORD}\n"
-    printf "# hpw: ${HPW}\n"
-    printf "# spw: ${SPW}\n"
-    printf "# psk: ${PSK}\n\n"
-    unzip -p openvpn.zip *_l3.ovpn | sed '/^#/d;s/\r//;/^$/d'
-
-    export PASSWORD='**'
-    vpnserver stop 2>&1 > /dev/null
-
-    # while-loop to wait until server goes away
-    set +e && while pgrep vpnserver > /dev/null; do sleep 1; done && set -e
-fi
-
-exec vpnserver execsvc &
-tail -F /var/log/vpnserver/*_log/*.log 
-exit $?
